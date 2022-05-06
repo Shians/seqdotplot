b683a4e5808595bb9b81955baa5948d3f062ada0
Shians <registertonysu@gmail.com>
2022-05-06 13:17:00 +1000

    Removed Rproj file

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -2,6 +2,6 @@
 # Generator token: 10BE3573-1514-4C36-9D1C-5A225CD40393
 
 compute_dotplot_data <- function(r_s1, r_s2, wsize, step, n_mismatches) {
-    .Call('_seqdotplot_compute_dotplot_data', PACKAGE = 'seqdotplot', r_s1, r_s2, wsize, step, n_mismatches)
+    .Call(`_seqdotplot_compute_dotplot_data`, r_s1, r_s2, wsize, step, n_mismatches)
 }
 
